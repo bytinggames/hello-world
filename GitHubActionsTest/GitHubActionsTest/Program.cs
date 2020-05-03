34d4086a2bf2fd1a0b4970e825f095a1cd9d29ff
bytinggames <bytinggames@gmail.com>
2020-05-03 14:19:32 +0200

    test project

---
+++ GitHubActionsTest/GitHubActionsTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubActionsTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            throw new Exception("nooo");
+        }
+    }
+}
---
+++ GitHubActionsTest/Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GitHubActionsTest;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Program.Main(new string[0]);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Program.Main(new string[] { "heeyy" });
+        }
+    }
+}
